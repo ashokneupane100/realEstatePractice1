1dee566e714dd3b154e775906c96536cb737d05e
ashokneupane100 <ashokabrother@gmail.com>
2024-10-08 14:35:23 +0545

    Navigation menu made best of the best

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <header className="bg-slate-200 shadow-md">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
+        
+        {/* Logo and Branding */}
         <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <h1 className="font-bold text-sm sm:text-xl flex items-center">
             <span className="text-slate-500">CodingCleverly</span>
             <span className="text-slate-800 ml-1">Estate</span>
           </h1>
@@ -16,25 +18,26 @@ export const Header = () => {
         <form className="relative flex items-center">
           <input
             type="text"
-            className="bg-white rounded-full px-4 py-2 pl-10 focus:outline-none w-24 sm:w-64 shadow-inner"
+            className="bg-white rounded-full px-4 py-2 pl-10 focus:outline-none w-32 sm:w-64 shadow-inner"
             placeholder="Search the address ..."
           />
-          <FaSearchLocation className="absolute left-3 text-slate-600" />
+          <FaSearchLocation className="absolute left-3 text-slate-600 text-lg" />
         </form>
-        <ul className="flex gap-4">
-          <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer ">
+
+        {/* Navigation Links */}
+        <ul className="flex gap-6 items-center">
+          <Link to="/" className="hidden sm:inline-block">
+            <li className="text-slate-700 hover:text-blue-500 transition duration-300 cursor-pointer">
               Home
             </li>
           </Link>
-          <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer ">
+          <Link to="/about" className="hidden sm:inline-block">
+            <li className="text-slate-700 hover:text-blue-500 transition duration-300 cursor-pointer">
               About
             </li>
           </Link>
-          <Link to="/sign-in">
-            <li className="sm:inline text-slate-700 hover:underline cursor-pointer ">
-              {" "}
+          <Link to="/sign-in" className="inline-block">
+            <li className="text-slate-700 hover:text-blue-500 transition duration-300 cursor-pointer">
               Sign In
             </li>
           </Link>
