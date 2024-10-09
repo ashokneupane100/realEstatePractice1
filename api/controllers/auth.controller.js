1950bb1483ad9b334ab8eb2611563b93cd45b1eb
ashokneupane100 <ashokabrother@gmail.com>
2024-10-09 17:41:07 +0545

    Sign up functionality done Perfectly

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,20 +1,28 @@
 import User from "../models/user.models.js";
 import bcryptjs from "bcryptjs";
-import { errorHandler } from "../utils/error.js";
 
-
-export const signup = async (req, res,next) => {
+export const signup = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
 
-    const hashedPassword= bcryptjs.hashSync(password,10);
+    // Check if the user already exists (duplicate email or username)
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res.status(409).json({
+        success: false,
+        message: "Username or email is already taken, you better go and login",
+      });
+    }
 
-    const user = new User({ username, email, password:hashedPassword });
+    // Hash the password and create a new user
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const user = new User({ username, email, password: hashedPassword });
 
     await user.save();
 
-    return res.status(201).json({message:"User has been created",user});
+    return res.status(201).json({ message: "User has been created", user });
   } catch (error) {
+    // Pass error to error handling middleware
     next(error);
   }
 };
