1b2b13ba0c7763506331c8833f47ead4ad4f3720
ashokneupane100 <ashokabrother@gmail.com>
2024-10-09 20:09:52 +0545

    Sign in Route made and UI also made

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,10 @@
 import User from "../models/user.models.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import env from "dotenv";
+
+env.config();
 
 export const signup = async (req, res, next) => {
   try {
@@ -26,3 +31,37 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    try {
+      const validUser = await User.findOne({ email });
+      if (!validUser) {
+        return next(errorHandler(404, "User not found"));
+      }
+  
+      const validPassword = await bcryptjs.compare(password, validUser.password);
+  
+      if (!validPassword) {
+        return next(errorHandler(401, "Invalid credentials"));
+      }
+  
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+  
+      // Destructure password out of validUser
+      const { password: pass, ...rest } = validUser._doc;
+  
+      // Use status 200 for a successful login
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .status(200) // Changed from 201 to 200
+        .json({ message: "Login success", user: rest }); // Send rest of the user data without password
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  };
+  
